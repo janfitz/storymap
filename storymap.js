c7e100663f1edac56bc587a1c01ecec02806d276
Jan Fitz <janfitz@localhost.localdomain>
2017-05-30 14:56:33 +0200

    churches commit

--- storymap.js
+++ storymap.js
@@ -9,19 +9,25 @@
             breakpointPos: '33.333%',
             createMap: function () {
                 // create a map in the "map" div, set the view to a given place and zoom
-                var map = L.map('map').setView([65, 18], 5);
+                var map = L.map('map', {
+                    center: [49.194908, 16.609784],
+                    zoom: 15
+                });
 
-                // add an OpenStreetMap tile layer
-                L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
-                    attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-                }).addTo(map);
+                var baselayers = {
+                    "B&W map": L.tileLayer('http://{s}.tiles.wmflabs.org/bw-mapnik/{z}/{x}/{y}.png'),
+                    "Mapnik": L.tileLayer('http://{s}.tile.openstreetmap.de/tiles/osmde/{z}/{x}/{y}.png'),
+                };
+
+                L.control.layers(baselayers).addTo(map);
+
+                L.tileLayer('http://{s}.tiles.wmflabs.org/bw-mapnik/{z}/{x}/{y}.png').addTo(map);
 
                 return map;
             }
         };
 
         var settings = $.extend(defaults, options);
-
 
         if (typeof(L) === 'undefined') {
             throw new Error('Storymap requires Laeaflet');
@@ -106,12 +112,44 @@
 
             var fg = L.featureGroup().addTo(map);
 
+            var churchesGroup = L.layerGroup();
+            var jakub = L.marker([49.196572, 16.608261]).addTo(churchesGroup);
+            var jan = L.marker([49.194299, 16.611152]).addTo(churchesGroup);
+            var janAmos = L.marker([49.197679, 16.603257]).addTo(churchesGroup);
+            var tomas = L.marker([49.197942, 16.607776]).addTo(churchesGroup);
+
+            jakub.bindPopup("Kostel sv. Jakuba");
+            jakub.on('click', function (e) {
+                this.openPopup();
+            });
+
+            jan.bindPopup("Kostel sv. Janů");
+            jan.on('click', function (e) {
+                this.openPopup();
+            });
+
+            janAmos.bindPopup("Českobratrský evangelický chrám Jana Amose Komenského");
+            janAmos.on('click', function (e) {
+                this.openPopup();
+            });
+
+            tomas.bindPopup("Kostel sv. Tomáše");
+            tomas.on('click', function (e) {
+                this.openPopup();
+            });
+
+            churchesGroup.addTo(map);
+
             function showMapView(key) {
 
                 fg.clearLayers();
+
                 if (key === 'overview') {
                     map.setView(initPoint, initZoom, true);
-                } else if (markers[key]) {
+                    churchesGroup.addTo(map);
+                }
+                else if (markers[key]) {
+                    churchesGroup.clearLayers();
                     var marker = markers[key];
                     var layer = marker.layer;
                     if(typeof layer !== 'undefined'){
@@ -120,8 +158,23 @@
                     fg.addLayer(L.marker([marker.lat, marker.lon]));
 
                     map.setView([marker.lat, marker.lon], marker.zoom, 1);
-                }
 
+                    if (key === 'jakub') {
+                      L.tileLayer('http://{s}.tile.openstreetmap.de/tiles/osmde/{z}/{x}/{y}.png').addTo(map);
+                    }
+                    else if (key === 'jan') {
+                      L.tileLayer('http://{s}.tiles.wmflabs.org/bw-mapnik/{z}/{x}/{y}.png').addTo(map);
+                    }
+                    else if (key === 'janAmos') {
+                      L.tileLayer('http://{s}.tile.openstreetmap.de/tiles/osmde/{z}/{x}/{y}.png').addTo(map);
+                    }
+                    else if (key === 'tomas'){
+                      L.tileLayer('http://{s}.tiles.wmflabs.org/bw-mapnik/{z}/{x}/{y}.png').addTo(map);
+                    }
+                }
+                else {
+                  fg.clearLayers();
+                }
             }
 
             paragraphs.on('viewing', function () {
