e95084d949cfbcbb3469fc1e5462c08b02ddc64c
Jan Fitz <janfitz@localhost.localdomain>
2017-05-31 14:00:28 +0200

    new markers

--- readme.md
+++ readme.md
@@ -1,10 +1,12 @@
-Brno Churches story map
-========
-
-Storymap is a jQuery-plugin to create a map that follows your text. Annotate each paragraph and place a map alongside it. Then you can zoom/pan/add marker etc to the map as the reader reads through the text.
+# Story map
+Storymap is a jQuery-plugin to create a map that follows your text. Annotate each paragraph and place a map alongside it. Then you can zoom/pan/add marker etc to the map as the reader reads through the text. Based on opensource JS [leaflet library](http://leafletjs.com/)
 
 Forked template from:  [atlefren/storymap](https://github.com/atlefren/storymap)
 
-License
--------
+## Used extensions
+- Color markers: https://github.com/pointhi/leaflet-color-markers
+- Smoothscroll: https://github.com/iamdustan/smoothscroll
+- Source texts: http://wikipedia.org
+
+## License
 Licensed under the MIT license.
--- storymap.js
+++ storymap.js
@@ -8,20 +8,32 @@
             selector: '[data-place]',
             breakpointPos: '33.333%',
             createMap: function () {
+
                 // create a map in the "map" div, set the view to a given place and zoom
                 var map = L.map('map', {
                     center: [49.194908, 16.609784],
                     zoom: 15
                 });
 
+                // Add some types of maps
                 var baselayers = {
-                    "B&W map": L.tileLayer('http://{s}.tiles.wmflabs.org/bw-mapnik/{z}/{x}/{y}.png'),
-                    "Mapnik": L.tileLayer('http://{s}.tile.openstreetmap.de/tiles/osmde/{z}/{x}/{y}.png'),
+                    "B&W map (výchozí)": L.tileLayer('http://{s}.tiles.wmflabs.org/bw-mapnik/{z}/{x}/{y}.png'),
+                    "Color map": L.tileLayer('http://{s}.tile.openstreetmap.de/tiles/osmde/{z}/{x}/{y}.png'),
+                    "Historical map": L.tileLayer('http://{s}.tile.thunderforest.com/pioneer/{z}/{x}/{y}.png'),
+                    "3D map": L.tileLayer('http://{s}.tile.openstreetmap.se/hydda/base/{z}/{x}/{y}.png')
                 };
-
                 L.control.layers(baselayers).addTo(map);
 
+                // Set default map style - tile
                 L.tileLayer('http://{s}.tiles.wmflabs.org/bw-mapnik/{z}/{x}/{y}.png').addTo(map);
+
+                // Set default map markers as one layer
+                var churchesGroup = L.layerGroup();
+                var jakub = L.marker([49.196572, 16.608261]).on('click', L.bind(getFocus, null, "jakub")).addTo(churchesGroup);
+                var jan = L.marker([49.194299, 16.611152]).on('click', L.bind(getFocus, null, "jan")).addTo(churchesGroup);
+                var janAmos = L.marker([49.197679, 16.603257]).on('click', L.bind(getFocus, null, "janAmos")).addTo(churchesGroup);
+                var tomas = L.marker([49.197942, 16.607776]).on('click', L.bind(getFocus, null, "tomas")).addTo(churchesGroup);
+                churchesGroup.addTo(map);
 
                 return map;
             }
@@ -29,31 +41,18 @@
 
         var settings = $.extend(defaults, options);
 
-        if (typeof(L) === 'undefined') {
-            throw new Error('Storymap requires Laeaflet');
-        }
-        if (typeof(_) === 'undefined') {
-            throw new Error('Storymap requires underscore.js');
-        }
-
         function getDistanceToTop(elem, top) {
             var docViewTop = $(window).scrollTop();
-
             var elemTop = $(elem).offset().top;
-
             var dist = elemTop - docViewTop;
-
             var d1 = top - dist;
-
             if (d1 < 0) {
                 return $(document).height();
             }
             return d1;
-
         }
 
         function highlightTopPara(paragraphs, top) {
-
             var distances = _.map(paragraphs, function (element) {
                 var dist = getDistanceToTop(element, top);
                 return {el: $(element), distance: dist};
@@ -90,9 +89,7 @@
             $('body').append(topElem);
 
             var top = topElem.offset().top - $(window).scrollTop();
-
             var searchfor = settings.selector;
-
             var paragraphs = element.find(searchfor);
 
             paragraphs.on('viewing', function () {
@@ -104,61 +101,50 @@
             });
 
             watchHighlight(element, searchfor, top);
-
             var map = settings.createMap();
+
+            var fg = L.featureGroup().addTo(map);
 
             var initPoint = map.getCenter();
             var initZoom = map.getZoom();
 
-            var fg = L.featureGroup().addTo(map);
-
-            var churchesGroup = L.layerGroup();
-            var jakub = L.marker([49.196572, 16.608261]).addTo(churchesGroup);
-            var jan = L.marker([49.194299, 16.611152]).addTo(churchesGroup);
-            var janAmos = L.marker([49.197679, 16.603257]).addTo(churchesGroup);
-            var tomas = L.marker([49.197942, 16.607776]).addTo(churchesGroup);
-
-            jakub.bindPopup("Kostel sv. Jakuba");
-            jakub.on('click', function (e) {
-                this.openPopup();
-            });
-
-            jan.bindPopup("Kostel sv. Janů");
-            jan.on('click', function (e) {
-                this.openPopup();
-            });
-
-            janAmos.bindPopup("Českobratrský evangelický chrám Jana Amose Komenského");
-            janAmos.on('click', function (e) {
-                this.openPopup();
-            });
-
-            tomas.bindPopup("Kostel sv. Tomáše");
-            tomas.on('click', function (e) {
-                this.openPopup();
-            });
-
-            churchesGroup.addTo(map);
-
             function showMapView(key) {
-
                 fg.clearLayers();
 
                 if (key === 'overview') {
                     map.setView(initPoint, initZoom, true);
-                    churchesGroup.addTo(map);
+
+                    /* Popup binding*/
+                    /*
+                    jakub.on('click', function (e) {
+                        this.openPopup();
+                    });
+
+                    jan.on('click', function (e) {
+                        this.openPopup();
+                    });
+
+                    janAmos.on('click', function (e) {
+                        this.openPopup();
+                    });
+
+                    tomas.on('click', function (e) {
+                        this.openPopup();
+                    });
+                    */
                 }
                 else if (markers[key]) {
-                    churchesGroup.clearLayers();
                     var marker = markers[key];
                     var layer = marker.layer;
                     if(typeof layer !== 'undefined'){
                       fg.addLayer(layer);
                     };
-                    fg.addLayer(L.marker([marker.lat, marker.lon]).on('click', L.bind(getFocus, null, key)));
+                    fg.addLayer(L.marker([marker.lat, marker.lon], {icon: redIcon}).on('click', L.bind(getFocus, null, key)));
 
                     map.setView([marker.lat, marker.lon], marker.zoom, 1);
 
+                    /* Custom map for each slide*/
+                    /*
                     if (key === 'jakub') {
                       L.tileLayer('http://{s}.tile.openstreetmap.de/tiles/osmde/{z}/{x}/{y}.png').addTo(map);
                     }
@@ -171,17 +157,15 @@
                     else if (key === 'tomas') {
                       L.tileLayer('http://{s}.tiles.wmflabs.org/bw-mapnik/{z}/{x}/{y}.png').addTo(map);
                     }
-                }
-                else {
-                  fg.clearLayers();
+                    */
                 }
             }
-
             paragraphs.on('viewing', function () {
                 showMapView($(this).data('place'));
             });
         };
 
+        // Focus on each slide after click event
         function getFocus(key) {
           document.getElementById(key).scrollIntoView({
             block: "start",
